735afdfbba4e341514d2fc2ab8d821d19e843f1d
joehannesburg <yusuf.hassen786@gmail.com>
2024-05-27 04:18:22 +0200

    deploy test 4

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 //Pages
-import Home from './pages/home/home';
-import About from './pages/about/about'
+import Home from './pages/home/Home';
+import About from './pages/about/About'
 
 //Components
 import Navbar from './components/navbar/navbar';
@@ -14,14 +14,14 @@ import Footer from './components/footer/Footer';
 function App() {
   return (
     <React.Fragment>
-    <BrowserRouter basename='/Yusuf_2503421_Assignment3'>
+    <BrowserRouter basename="/Yusuf_2503421_Assignment3">
         <Navbar />
         <Routes>
           {/* Routing for pages */}
       
           <Route index element = {<Home/>}/>
-          <Route path="/" element={<Home/>} />
-          <Route path="/about" element={<About/>} />
+          <Route path="/" exact element={<Home/>} />
+          <Route path="/about" exact element={<About/>} />
           {/* <Route path="/design" element={<TicketPurchase />} />
           <Route path="/theory" element={<ConfirmationPage />} />
           <Route path="/art" element={<ConfirmationPage />} />  */}
